08bb3e423bc695ac4f20d7bd89aa9b695e5af5e5
Frank Pinder <fpinder@comcast.net>
2020-02-04 19:29:54 -0800

    changed heroku connections

--- models/Note.js
+++ models/Note.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 // Create a schema class
 var Schema = mongoose.Schema;
 
-/* Create the Note schema */
+// Create the Note schema */
 var NoteSchema = new Schema({
     body: {
         type: String
