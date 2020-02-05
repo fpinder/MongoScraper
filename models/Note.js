04101f7225d95140ab446dbff59c95a3253764f4
Frank Pinder <fpinder@comcast.net>
2020-02-04 19:28:54 -0800

    testing heroku connections

--- models/Note.js
+++ models/Note.js
@@ -14,7 +14,7 @@ var NoteSchema = new Schema({
     }
 });
 
-/* Create the Note model with the NoteSchema */
+// Create the Note model with the NoteSchema
 var Note = mongoose.model("Note", NoteSchema);
 
 // Export the Note model
